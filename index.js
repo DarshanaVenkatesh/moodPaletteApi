6d4bb907fbf7d5155be173fe371db3c6cd9de786
DarshanaVenkatesh <venkat82@purdue.edu>
2023-04-28 16:42:05 -0400

    changed cors permissions:

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(
 
 const cors = require('cors')
 const corsOptions ={
-    origin:'http://localhost:8800', 
+    origin:['https://moodpalette-api.onrender.com', 'https://moodpalette.onrender.com'],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
